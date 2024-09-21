a350b7abd6fdcd723db82e9ab18f22b41e416588
anurag <anurag@kali>
2024-09-21 14:07:50 +0530

    Connecting to database

--- url_shortner.php
+++ url_shortner.php
@@ -4,6 +4,29 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+    <p>
+    <input placeholder="Enter your url here" value=""/>
+    <button type="button">Shorten URL</button> 
+    <?php
+    $server_name = "localhost";
+    $user_name= "urlreader";
+    $password="reader@123";
+    $dbname="urlShortner";
+
+    try
+    {
+        $conn= new PDO("mysql:host=$server_name;dbname=$dbname",$user_name,$password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO all_url(url,shortend_url) VALUES ('https://www.google.com/','https://www.example.com')";
+        $conn->exec($sql);
+
+    }
+    catch(PDOException $e)
+    {
+        echo $sql . "<br>". $e->getMessage();
+    }
+    
+    ?>  
+    </p>    
     </body>
 </html>
