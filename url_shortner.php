65a5ff3982414bd60ed595462b66610d2822550f
anurag <anurag@kali>
2024-09-26 20:11:40 +0530

    Modifiying Insert Statement with Prepared Statement

--- url_shortner.php
+++ url_shortner.php
@@ -26,10 +26,15 @@
 
     try
     {
-        $conn= new PDO("mysql:host=$server_name;dbname=$dbname",$user_name,$password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO all_url(url,shortend_url) VALUES ('https://www.google.com/','https://www.example.com')";
-        $conn->exec($sql);
+        function insertUrl()
+        {
+            $conn= new PDO("mysql:host=$server_name;dbname=$dbname",$user_name,$password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $sql = $conn-> prepare("INSERT INTO all_url(url,shortend_url) VALUES (:url,:shortUrl)");
+            $sql->bindparam(':url',$inputUrl);
+            $sql->bindparam(':shortUrl',$shortedUrl);
+            $sql->execute();
+        }
 
     }
     catch(PDOException $e)
@@ -50,7 +55,7 @@
         } 
         catch(PDOException $e){echo "Error:".$e->getMessage();}
        
-    
+        $conn=null;
     // }
     ?>  
     </p>  
